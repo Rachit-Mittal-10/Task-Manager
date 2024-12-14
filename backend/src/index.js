81718511812a52ecdf32934e7f82ca1d0bb0100f
Rachit <mittalrachit10063003@gmail.com>
2024-12-15 00:40:42 +0530

    cors and json parser added in app backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,20 @@
+//* importing the modules
 import express from "express";
 import { config } from "dotenv";
+import cors from 'cors';
 import conn from "./config/mysql.js";
 
+//* Reading the environment file
 const env = config({
     path: "./.env"
 });
 
+//* Getting the App from Express
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-
-
+//* Listening on port
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
     console.log(`Server is listening on ${PORT}`);
