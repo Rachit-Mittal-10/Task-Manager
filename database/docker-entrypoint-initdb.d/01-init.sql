0b08d1a4028e14111bb4cee0c71c1a7b6c5ebb9e
Rachit <mittalrachit10063003@gmail.com>
2024-12-16 14:56:25 +0530

    few info about db changed

--- database/docker-entrypoint-initdb.d/01-init.sql
+++ database/docker-entrypoint-initdb.d/01-init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users(
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password_hashed VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -17,13 +17,13 @@ CREATE TABLE IF NOT EXISTS tasks(
     start_time DATETIME,
     end_time DATETIME,
     task_status ENUM('planned','pending','finished') NOT NULL,
-    priority ENUM('low','medium','high','not set') NOT NULL,
+    priority ENUM('low','medium','high','not set') NOT NULL DEFAULT 'not set',
     description VARCHAR(512),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS user_task_mapping(
+CREATE TABLE IF NOT EXISTS mapping(
     user_id INT NOT NULL,
     task_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
