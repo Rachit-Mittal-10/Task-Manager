dac25b4b73429c2c331af982fcb0649f6c5302da
Rachit <mittalrachit10063003@gmail.com>
2024-12-17 02:21:26 +0530

    Refactoring of files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,8 @@
 import express from "express";
-import { config } from "dotenv";
 import cors from 'cors';
 import { router as authRouter } from "./routes/authRouter.js";
 import { authenticateToken } from "./middleware/authMiddleware.js";
 import { router as taskRouter } from "./routes/taskRouter.js";
-
-const env = config({
-    path: "./.env"
-});
-
 
 //* Getting the App from Express and adding cors and json middleware
 const app = express();
@@ -17,14 +11,8 @@ app.use(express.json());
 
 
 //* App routes
-app.use("/user", authRouter);
-app.use("/task",authenticateToken,taskRouter);
+app.use("/auth", authRouter);
+app.use("/tasks",authenticateToken,taskRouter);
 
 
-//* Listening on port
-const PORT = process.env.PORT;
-app.listen(PORT, ()=>{
-    console.log(`Server is listening on ${PORT}`);
-});
-
-// app.listen(PORT);
+export default app;
---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import app from "./app.js";
+import { config } from "dotenv";
+
+const env = config({
+    path: "./.env"
+});
+
+//* Listening on port
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`);
+});
+
+// app.listen(PORT);
