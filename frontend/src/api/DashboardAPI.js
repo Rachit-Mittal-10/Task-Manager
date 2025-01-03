5e326a6523b1361742b06b43271f40d964aaadb5
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-01-04 03:05:39 +0530

    function to call the api written

---
+++ frontend/src/api/DashboardAPI.js
@@ -0,0 +1,23 @@
+import API from "./axiosInstance";
+
+const DashboardAPI = (()=>{
+    const getDashboard = async () => {
+        try{
+            const data = await API.get("/");
+            return data;
+        }
+        catch(err){
+            if(err.response){
+                return err.response.data;
+            }
+            else{
+                console.log(err);
+            }
+        }
+    };
+    return {
+        getDashboard,
+    };
+})();
+
+export default DashboardAPI;
