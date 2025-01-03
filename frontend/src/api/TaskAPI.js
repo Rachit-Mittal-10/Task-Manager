22e79da935168de7ae9b19aa3e48988828cb2248
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-01-04 03:06:36 +0530

    useless import removed

--- frontend/src/api/TaskAPI.js
+++ frontend/src/api/TaskAPI.js
@@ -1,6 +1,4 @@
 import API from "./axiosInstance";
-import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
 
 const TaskAPI = (() => {
     const createTask = async (tasksData) => {
