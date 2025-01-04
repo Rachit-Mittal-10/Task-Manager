95aaeff9088dcc8023fd8bfca898858b243bcb14
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-01-05 01:52:14 +0530

    Header component and table component created

---
+++ frontend/src/Components/Table.jsx
@@ -0,0 +1,26 @@
+const Table = (props) => {
+    const data = props.data;
+    const columns = props.columns;
+    return (
+        <table border="1" style={{width: "100%", borderCollapse: "collapse"}}>
+            <thead>
+                <tr>
+                    {columns.map((columns, index)=>(
+                        <th key={index}>{columns.label}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((row, index)=>(
+                    <tr key={index}>
+                        {columns.map((column, index)=>(
+                            <td key={index}>{row[column.key]}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
