573be13fde88c17625b25144adf5a8d94bf9f1f3
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-01-07 22:15:24 +0530

    edit dialog css written and form included. onChange and onSubmit to be written

--- frontend/src/Components/EditDialog.jsx
+++ frontend/src/Components/EditDialog.jsx
@@ -41,15 +41,50 @@ const EditDialog = (props) => {
         setDialogData({});
     };
 
+    const onInputChange = () => {};
+    const onSubmitClick = () => {};
+
     return (
-        <dialog ref={dialogRef} >
-            <div>
-                <button onClick={handleCloseButtonClick}>
-                    <img src={close} alt="close button" />
+        <dialog ref={dialogRef} className={styles.editDialog} >
+            <div className={styles.header}>
+                <button onClick={handleCloseButtonClick} className={styles.closeButton}>
+                    <img src={close} alt="close button" className={styles.closeImage} />
                 </button>
             </div>
-            <div>
-                <p>{JSON.stringify(dialogData)}</p>
+            <div className={styles.dataWrapper}>
+                <form>
+                    <div>
+                        <label htmlFor="id">ID:</label>
+                        <input type="number" id="id" name="id" value={dialogData.data.id} />
+                    </div>
+                    <div>
+                        <label htmlFor="title">Title:</label>
+                        <input type="text" id="title" name="title" value={dialogData.data.title} />
+                    </div>
+                    <div>
+                        <label htmlFor="status">Status:</label>
+                        <input type="text" id="status" name="status" value={dialogData.data.status} />
+                    </div>
+                    <div>
+                        <label htmlFor="priority">Priority:</label>
+                        <input type="text" id="priority" name="priority" value={dialogData.data.priority} />
+                    </div>
+                    <div>
+                        <label htmlFor="start_time">Start Time:</label>
+                        <input type="date" id="start_time" name="start_time" value={dialogData.data.start_time} />
+                    </div>
+                    <div>
+                        <label htmlFor="end_time">End Time:</label>
+                        <input type="date" id="end_time" name="end_time" value={dialogData.data.end_time} />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description:</label>
+                        <input type="text" id="description" name="description" value={dialogData.data.description} />
+                    </div>
+                    <div className={styles.submitWrapper}>
+                        <button className={styles.submitButton}>Submit</button>
+                    </div>
+                </form>
             </div>
         </dialog>
     );
