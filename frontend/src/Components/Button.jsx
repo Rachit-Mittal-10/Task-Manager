1b774de59f7463e8065da214f16ad374e021e7da
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-01-13 00:28:28 +0530

    button component created

---
+++ frontend/src/Components/Button.jsx
@@ -0,0 +1,16 @@
+import styles from "./Button.module.scss";
+
+const Button = (props) => {
+    const text=props.text;
+    const onClick=props.onClick;
+    const type=props.type;
+    return (
+        <>
+            <button className={styles.Button} type={type} onClick={onClick}>
+                {text}
+            </button>
+        </>
+    );
+};
+
+export default Button;
