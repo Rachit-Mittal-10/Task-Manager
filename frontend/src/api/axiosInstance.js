1ab0d09b9941dc1d3ab064967dd2ac341c51648f
Rachit <rachit.mittal@avaloninfosys.com>
2025-01-20 15:06:33 +0530

    dd

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //* Storing some common thing and creating the instance
 const API = axios.create({
-    baseURL: `http://127.0.0.1:3000`,
+    baseURL: `http://backend:3000`,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
