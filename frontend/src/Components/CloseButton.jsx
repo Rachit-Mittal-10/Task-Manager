0f79af42cedf6f8753ddf46f7c82b6ca68d4dad8
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-02-02 21:50:50 +0530

    Close Button Component created for modularity

---
+++ frontend/src/Components/CloseButton.jsx
@@ -0,0 +1,20 @@
+import styles from "./CloseButton.module.scss";
+import close from "../assets/images/close.png";
+
+const CloseButton = () => {
+    return (
+        <>
+            <button
+                onClick={props.onClick}
+                className={styles.button}
+            >
+                <img
+                    src={close}
+                    alt="Close Button"
+                />
+            </button>
+        </>
+    );
+};
+
+export default CloseButton;
