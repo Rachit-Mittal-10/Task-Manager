457ef616fa912f19046092dfe17adffc53a44575
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-02-04 23:35:39 +0530

    button

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -1,13 +1,14 @@
 import styles from "./Button.module.scss";
 
 const Button = (props) => {
-    const text = props.text;
+    // const text = props.text;
     const onClick = props.onClick;
     const type = props.type;
+    const children = props.children;
     return (
         <>
             <button className={styles.Button} type={type} onClick={onClick}>
-                {text}
+                {children}
             </button>
         </>
     );
