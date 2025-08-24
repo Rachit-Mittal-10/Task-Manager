6f91f6d565b405808410d7d5fc87ef6c1000d235
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-08-24 18:44:50 +0530

    sync

--- frontend/src/features/Logout/Logout.jsx
+++ frontend/src/features/Logout/Logout.jsx
@@ -1,11 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import AuthAPI from "../../api/AuthAPI";
 
 const Logout = () => {
-    const { setToken, setUser, setIsAuthenticated } = useAuth();
+    const { logout } = useAuth();
     const navigate = useNavigate();
-    AuthAPI.logout({setToken,setUser,setIsAuthenticated});
+    logout();
     navigate("/login", { replace: true });
 };
 
