d26b247e0c902634d4e609096b6c92601570cc11
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-10-05 02:34:42 +0530

    yay alias are working

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "../context/AuthContext";
+import { AuthProvider } from "@context/AuthContext";
 import Main from "./layout/Main/Main";
 import Header from "./layout/Header/Header";
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,8 +14,14 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@': './src',
-            '@assets': 
+            '@': path.resolve(__dirname,'./src'),
+            '@app': path.resolve(__dirname,'./src/app'),
+            '@assets': path.resolve(__dirname,'./src/assets'),
+            '@components': path.resolve(__dirname,'./src/components'),
+            '@api': path.resolve(__dirname,'./src/api'),
+            '@context': path.resolve(__dirname,'./src/context'),
+            '@features': path.resolve(__dirname,'./src/features'),
+            '@utils': path.resolve(__dirname,'./src/utils'),
         }
     }
 });
