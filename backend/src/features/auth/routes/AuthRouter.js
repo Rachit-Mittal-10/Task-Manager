4a590988dee3b196c7b13c93a8236d1eae964415
Rachit <mittalrachit10063003@gmail.com>
2025-10-13 16:20:48 +0530

    delete routes and starting from start

--- backend/src/features/auth/routes/AuthRouter.js
+++ backend/src/features/auth/routes/AuthRouter.js
@@ -1,10 +1,1 @@
-import { Router } from "express";
-import { login, register } from "../controllers/authController.js";
 
-const router = Router();
-
-router.post("/login", login);
-
-router.post("/register", register);
-
-export { router };
