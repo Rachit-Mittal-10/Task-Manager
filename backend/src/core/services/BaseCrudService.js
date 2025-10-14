03ab9d4272a5b4b995a7b7fdecbbb5533b1a8465
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-10-15 01:05:47 +0530

    base crud and base service

---
+++ backend/src/core/services/BaseCrudService.js
@@ -0,0 +1,68 @@
+import BaseService from "./BaseService";
+/*
+* BaseCrudService.js
+* This provides the basic functionality for all the application service layers
+*/
+class BaseCrudService extends BaseService {
+    #model;
+    constructor(model, dep = {}){
+        if(!model){
+            throw new Error("Model is mandatory!!!")
+        }
+        this.#model = model;
+        super(dep);
+    }
+    async create(data){
+        if(!data || Object.keys(data).length === 0){
+            throw new Error("Data is empty");
+        }
+        try {
+            const result = await this.#model.create(data);
+            return result;
+        }
+        catch (err){
+            throw err;
+        }
+    }
+    async get(id){
+        try {
+            const result = await this.#model.get(id);
+            return result;
+        }
+        catch (err){
+            throw err;
+        }
+    }
+    async getAll(){
+        try {
+            const result = await this.#model.getAll();
+            return result;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+    async update(id,data){
+        if(!data || Object.keys(data) === 0){
+            throw new Error("Data is Empty");
+        }
+        try {
+            const result = await this.#model.update(id,data);
+            return result;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+    async remove(id){
+        try {
+            const result = await this.#model.remove(id);
+            return result;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
+};
+
+export default BaseCrudService;
--- backend/src/core/services/BaseService.js
+++ backend/src/core/services/BaseService.js
@@ -1,67 +1,10 @@
-/*
-* BaseService.js
-* This provides the basic functionality for all the application service layers
-*/
 class BaseService {
-    #model;
     #dep;
-    constructor(model, dep = {}){
-        if(!model){
-            throw new Error("Model is mandatory!!!")
-        }
-        this.#model = model;
+    constructor(dep = {}){
         this.#dep = dep;
     }
-    async create(data){
-        if(!data || Object.keys(data).length === 0){
-            throw new Error("Data is empty");
-        }
-        try {
-            const result = await this.#model.create(data);
-            return result;
-        }
-        catch (err){
-            throw err;
-        }
-    }
-    async get(id){
-        try {
-            const result = await this.#model.get(id);
-            return result;
-        }
-        catch (err){
-            throw err;
-        }
-    }
-    async getAll(){
-        try {
-            const result = await this.#model.getAll();
-            return result;
-        }
-        catch (err) {
-            throw err;
-        }
-    }
-    async update(id,data){
-        if(!data || Object.keys(data) === 0){
-            throw new Error("Data is Empty");
-        }
-        try {
-            const result = await this.#model.update(id,data);
-            return result;
-        }
-        catch (err) {
-            throw err;
-        }
-    }
-    async remove(id){
-        try {
-            const result = await this.#model.remove(id);
-            return result;
-        }
-        catch (err) {
-            throw err;
-        }
+    get dep(){
+        return this.dep;
     }
 };
 
