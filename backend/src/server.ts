05d929b22363150d012acb6cafdd6c182769eae1
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-12-10 23:53:19 +0530

    changed filename to ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,22 +2,19 @@ import { config } from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = path.dirname(__filename);
 // __dirname: {PROJECT_ROOT}/backend/src/
 // env file: {PROJECT_ROOT}/.env
-const env = config({
+const env:any = config({
     path: path.resolve(__dirname, "..", "..", ".env"),
 });
 
-// console.log(env);
-// console.log(`process.env in the server.js file:`);
-// console.log(process.env);
-// import App from "#app/App.js";
+// Dynamic Import
 const { default: App } = await import("#app/App.js");
 //* Listening on host:port
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+const PORT:any = process.env.PORT;
+const HOST:any = process.env.HOST;
 App.listen(PORT, HOST, () => {
     console.log(`Server is listening on ${HOST}:${PORT}`);
 });
