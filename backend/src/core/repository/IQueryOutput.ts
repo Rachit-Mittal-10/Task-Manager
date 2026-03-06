873ce06092443b83180108ceed15b1bdc484ff14
Rachit Mittal <mittalrachit10063003@gmail.com>
2026-03-07 03:04:11 +0530

    updated read write outpt

--- backend/src/core/repository/IQueryOutput.ts
+++ backend/src/core/repository/IQueryOutput.ts
@@ -1,11 +1,7 @@
+import { IData } from "#common/types/IData.js";
 import { FieldPacket, ResultSetHeader, RowDataPacket } from "mysql2/promise";
 
-export type WriteOutput = ResultSetHeader | ResultSetHeader[];
-export type ReadOutput = RowDataPacket[] | RowDataPacket[][];
+export type WriteOutput = number | number[];
+export type ReadOutput = IData[] | IData[][];
 
 export type QueryOutput = WriteOutput | ReadOutput;
-
-export type ExecuteOutput =[
-    QueryOutput,
-    FieldPacket | FieldPacket[]
-];
