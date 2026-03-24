2f336db7b4a2c2d4bd3ef8b6de4428b0b39b8960
Rachit Mittal <mittalrachit10063003@gmail.com>
2026-03-25 00:38:03 +0530

    Add optional extraFilter parameter to update and remove methods in IBaseCrudRepository

--- backend/src/core/repository/IBaseCrudRepository.ts
+++ backend/src/core/repository/IBaseCrudRepository.ts
@@ -4,6 +4,6 @@ import { IOptions } from "./BaseCrudRepository.js";
 export interface IBaseCrudRepository<T> {
     create(data: IData): Promise<number>;
     read(id?: number | undefined,filters?:IData | undefined, options?: IOptions): Promise<T | T[] | undefined>;
-    update(id: number, data:IData): Promise<number>;
-    remove(id: number): Promise<number>;
+    update(id: number, data:IData, extraFilter?: IData): Promise<number>;
+    remove(id: number, extraFilter?: IData): Promise<number>;
 };
