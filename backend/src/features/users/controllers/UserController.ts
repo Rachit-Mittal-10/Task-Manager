49e09ae68a02911e0b35adcd05d0e7813de97031
Rachit Mittal <mittalrachit10063003@gmail.com>
2026-04-07 23:07:02 +0530

    Add getRequestContext method to UserController for user ID retrieval

--- backend/src/features/users/controllers/UserController.ts
+++ backend/src/features/users/controllers/UserController.ts
@@ -1,5 +1,13 @@
 import { BaseCrudController } from "#core/controllers/BaseCrudController.js";
 import type { UserModel } from "../models/UserModel.js";
 import { UserService } from "../services/UserService.js";
+import { Request } from "express";
 
-export class UserController extends BaseCrudController<UserModel, UserService> {}
+export class UserController extends BaseCrudController<UserModel, UserService> {
+    protected async getRequestContext(request: Request): Promise<any> {
+        const context = await super.getRequestContext(request);
+        return {
+            id: context.user_id,
+        }
+    }
+};
